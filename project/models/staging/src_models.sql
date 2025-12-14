ec805f0d24cddf9b3a5195e4ff587bfc6c99594d
w7595wen <wingchee8250@gmail.com>
2025-12-14 12:18:01 +1100

    Initialized dbt project

---
+++ project/models/staging/src_models.sql
@@ -0,0 +1,13 @@
+    
+    -- "RAW_MOVIE" is the table created from scratch inside folder "RAW"
+    -- "MOVIELENS" is the database name
+    -- "RAW" is the folder inside database "MOVIELENS"
+    -- "RAW_MOVIE" is the table created from scratch inside folder "RAW"
+    WITH raw_movies AS(
+        SELECT * FROM MOVIELENS.RAW.RAW_MOVIES
+    )
+    SELECT
+        movieID AS movie_id,
+        title,
+        genre 
+    FROM raw_movies
