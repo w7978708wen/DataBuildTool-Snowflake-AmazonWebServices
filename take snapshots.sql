2cc9856b2ba7153b339950fe4784c01f4d6f7e28
w7978708wen <wingchee8250@gmail.com>
2025-12-28 17:38:34 +1100

    Update take snapshots.sql

--- take snapshots.sql
+++ take snapshots.sql
@@ -2,25 +2,25 @@ SELECT * FROM snapshots.snap_tags
 ORDER BY user_id, dbt_valid_from DESC;
 
 
--- UPDATE src_tags means modify existing rows in table src_tags. 
+-- UPDATE stage_tags means modify existing rows in table stage_tags. 
 -- UPDATE fcn only changes values in rows that alredy exist. 
--- SET tag = 'Mark Waters Returns' means change the tag column's value to 'Mark Waters Returns'  
+-- SET tag = 'dark comedy' means change the tag column's value to 'dark comedy'  
 -- regardless of what the old tag name was 
 -- tag_timestamp = CAST(CURRENT_TIMESTAMP() AS TIMESTAMP_NTZ) returns the current date & time when query runs
 -- CAST(... AS TIMESTAMP_NTZ) converts timestamp into TIMESTAMP_NTZ meaning timestamp with no time zone
--- WHERE user_id = 18 means only apply this to rows with user_id = 18
-UPDATE src_tags
-SET tag = 'Mark Waters Returns', tag_timestamp = CAST(CURRENT_TIMESTAMP() as TIMESTAMP_NTZ)
-WHERE user_id = 18;
+-- WHERE user_id = 121 means only apply this to rows with user_id = 121
+UPDATE stage_tags
+SET tag = 'dark comedy', tag_timestamp = CAST(CURRENT_TIMESTAMP() as TIMESTAMP_NTZ)
+WHERE user_id = 121;
 
 
--- View table with updated tag (for rows where user_id = 18)
-SELECT * from dev.src_tags
-WHERE user_id = 18;
+-- View table with updated tag (for rows where user_id = 121)
+SELECT * from dev.stage_tags
+WHERE user_id = 121
 ORDER BY user_id DESC;
 
 
 --You can see 2 versions of row before and after update. View snapshot table 
-SELECT * FROM snapshots.snap_tags
-WHERE user_id = 18
+SELECT * FROM snapshots.stage_tags
+WHERE user_id = 121
 ORDER BY user_id, dbt_valid_from DESC;
